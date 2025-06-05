cdd5b200eb4f8a9ef68792f0205e2320892e5b67
renzie-de-baerdemaeker <renzie.debaerdemaeker@student.hogent.be>
2025-06-05 11:16:40 +0200

    Executed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_20_092435) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_05_090624) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_20_092435) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "branding_id", null: false
+    t.index ["branding_id"], name: "index_admins_on_branding_id"
+  end
+
   create_table "brandings", force: :cascade do |t|
     t.string "name"
     t.string "primary_color"
@@ -51,4 +61,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_20_092435) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "admins", "brandings"
 end
