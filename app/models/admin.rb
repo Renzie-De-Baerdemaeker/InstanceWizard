663669b0a73b9112618dd54c31c8558fa220dcb9
renzie-de-baerdemaeker <renzie.debaerdemaeker@student.hogent.be>
2025-06-12 00:13:17 +0200

    Admin views cleanup

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ApplicationRecord
   belongs_to :branding
   validates :first_name, :last_name, :email, presence: true
-  validates :email, format: URI::MailTo::EMAIL_REGEXP
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
