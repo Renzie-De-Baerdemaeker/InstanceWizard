e48287f361ef95b76317269c47175d081fedbe48
renzie-de-baerdemaeker <renzie.debaerdemaeker@student.hogent.be>
2025-06-11 23:57:37 +0200

    Added validation for assets

--- app/models/branding.rb
+++ app/models/branding.rb
@@ -2,6 +2,11 @@ class Branding < ApplicationRecord
   has_one_attached :logo
   has_one_attached :favicon
   has_one_attached :background
+
   validates :name, presence: true
+  validates :logo, presence: true
+  validates :favicon, presence: true
+  validates :background, presence: true
+
   has_many :admins, dependent: :destroy
 end
